c67efbb29f47db1ab4fc7a5969efca993a4cb8bd
qinwen <ailsa.qin@chinaredstar.com>
2017-07-24 13:37:33 +0800

    http的相关demo

--- src/componentsdemo/home/router.tsx
+++ src/componentsdemo/home/router.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Route,Switch } from 'react-router-dom';
 import { HomeComponentHoc as HomeComponent } from './components/home';
-import { HttpDemoComponent } from '../http';
+import {RouterComponent as  HttpDemoRouter } from '../http';
 import { HeaderapiComponent } from '../header';
 
 
@@ -11,7 +11,7 @@ export class RouterComponent extends React.Component<any, any>{
     render() {
         return (
                 <Switch>
-                        <Route path="/components/http" component={HttpDemoComponent} />
+                        <Route path="/components/http" component={HttpDemoRouter} />
                         <Route path="/components/header" component={HeaderapiComponent} />
                         <Route path="/components" component={HomeComponent} />
                </Switch>
--- src/componentsdemo/http/components/http.tsx
+++ src/componentsdemo/http/components/http.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import { Link } from 'react-router-dom';
 import { hoc } from '../containers/http';
 import { IProps } from '../constants/http';
 
@@ -10,37 +10,10 @@ export class HttpDemoComponent extends React.Component<IProps, any> {
         return (
             <div className="http-demo-content">
                 <ul>
-                    <li>
-                        <h1>1、通过fetch 请求</h1>
-                        <div className="operation">
-                            <div className="choose-content"> 
-                                <label className="choose-title">Method</label>
-                                <label className="choose-label"><input name="method" type="radio" value="post" />post </label> 
-                                <label className="choose-label"><input name="method" type="radio" value="get" />get </label> 
-                            </div>
-                            <h3 className="btn" onClick={() => { this.props.http_get_noparam() }}>点击请求</h3>
-                        </div>
-                        {this.props.http_get_noparam_reault && <div>
-                            <h3>请求描述：</h3>
-                            <ul className="info">
-                                <li>
-                                    <span className="info-title">URL:</span>
-                                    <span className="info-detail">http://fc-c.dev.rs.com/parameter/loupan</span>
-                                </li>
-                                <li>
-                                    <span className="info-title">Method:</span>
-                                    <span className="info-detail">GET</span>
-                                </li>
-                            </ul>
-                        </div>
-                        }
-                        {this.props.http_get_noparam_reault &&
-                            <div className="result">
-                                <h3>结果:</h3>
-                                <span className="detail">{this.props.http_get_noparam_reault}</span>
-                            </div>
-                        }
-                    </li>
+                    <Link to={`/components/http/get-noheader-noparams`} ><li> get-无header-无参数 </li></Link>
+                    <Link to={`/components/http/get-noheader-params`} ><li> get-无header-有参数 </li></Link>
+                    <Link to={`/components/http/post_noheader_params`} ><li> post-无header-有参数 </li></Link>
+                    <Link to={`/components/http/post_header_params`} ><li> post-有header-有参数 </li></Link>
                 </ul>
             </div>
         );
